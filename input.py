91eb8409532668ce0f0eda901d2927e438e01f05
Tesla9000 <protonmail693@gmail.com>
2024-02-26 00:01:40 +0530

    expect Line

--- input.py
+++ input.py
@@ -1,4 +1,26 @@
 if True:
+      
+      dasda                
+      
+else:
+      a
+
+
+a
+
+
+
+
     a=3
+if True:
+            
+
+
+
+                  set
+
+
+
+^[ \t\f]*
 else:
       a=4

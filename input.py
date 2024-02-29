9692d20628911601bdd99cd0f8867aba958e3770
Tesla9000 <protonmail693@gmail.com>
2024-02-29 17:19:16 +0530

    delayed child

--- input.py
+++ input.py
@@ -1,16 +1,3 @@
-def bubbleSort(array: list[int]) -> None:
-  i: int = 0
-  for i in range(0,len(array)-1):
-    swapped: bool = False
-    for j in range(0, len(array) - i - 1):
-      if array[j] > array[j + 1]:
-        temp: int = array[j]
-        array[j] = array[j + 1]
-        array[j + 1] = temp
-        swapped = True
-    if not swapped:
-      break
-
 def main():
   data: list[int] = [-2, 45, 0, 11, -9]
   bubbleSort(data)

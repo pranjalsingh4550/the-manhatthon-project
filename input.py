be992045f40b723bb5e09442a09efafc362a99e2
pranjal <prsingh@cse.iitk.ac.in>
2024-02-29 13:15:38 +0530

    Remove ast-example.dot for autocomplete

--- input.py
+++ input.py
@@ -1,5 +1,45 @@
-for i in range(10):
-    print("nice")
+if a:
+	a;
+elif b:
+	b;
 else:
-    print("not nice")
-(a)=3
+	return c;
+
+
+return "strr example"
+
+a=4
+
+if a:   
+        a =2
+elif a:
+        a=2
+elif a:
+        a=2
+elif a:
+        a=2
+elif a:
+        a=2
+else:
+        a=3
+a=4
+
+def ifbl()->int:
+        if a:   
+                a =2
+        elif a:
+                a=2
+        elif a:
+                a=2
+        elif a:
+                a=2
+        elif a:
+                a=2
+                def nesteddef(a:int = 44, b:int = 43) ->str:
+                        pass
+        else:
+                a=3
+        return "string heree"
+a=4
+
+

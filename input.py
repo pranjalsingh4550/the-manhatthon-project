dcd53342cdaf85dbd9b5e2bba05f34bfaa0c886e
Tesla9000 <protonmail693@gmail.com>
2024-03-01 20:08:35 +0530

    multiline

--- input.py
+++ input.py
@@ -13,8 +13,7 @@ def bubbleSort(array: list[int]) -> None:
         break
 
 def main():
-    data: list[int] = [-2, 45, 0,
-                        11, -9]
+    data: list[int] = [-2, 45, 0,11, -9]
     bubbleSort(data)
 
     print('Sorted Array in Ascending Order:')
